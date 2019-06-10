6de493c60c00b2b3e1d7f28bb9b1beee4cf8edb7
Austin Vickers <adv0003@uah.edu>
2019-06-10 14:33:14 -0500

    Removed unnecessary usings

--- RabbitMQ Tutorial/RPClient/RPClientReciever.cs
+++ RabbitMQ Tutorial/RPClient/RPClientReciever.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Recieve;
-using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Collections.Concurrent;
 
--- RabbitMQ Tutorial/Send/Sender.cs
+++ RabbitMQ Tutorial/Send/Sender.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using RabbitMQ.Client;
-using Util;
 
 namespace Send
 {
