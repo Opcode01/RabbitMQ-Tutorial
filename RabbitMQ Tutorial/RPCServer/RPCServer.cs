ff2fc85ea3fec51118068067f92bc5b576447ab6
Austin Vickers <adv0003@uah.edu>
2019-06-10 11:32:47 -0500

    Initial setup

---
+++ RabbitMQ Tutorial/RPCServer/RPCServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCServer
+{
+    class RPCServer
+    {
+
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1) return n;
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
